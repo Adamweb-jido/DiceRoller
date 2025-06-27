4cc3eea667c49446c1fa2b4190c30e279dcd59bc
Adamweb-jido <realadamweb@gmail.com>
2025-06-27 18:41:04 +0100

    Friday Commit

--- app/src/main/java/com/adamweb/diceroller/MainActivity.kt
+++ app/src/main/java/com/adamweb/diceroller/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.adamweb.diceroller
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.adamweb.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.myText.setText("Welcome")
+
     }
 }
